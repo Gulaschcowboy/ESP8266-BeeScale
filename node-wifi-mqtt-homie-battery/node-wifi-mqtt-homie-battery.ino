ac2bde94061ff83905ecb98ff0af53942d8f9e3c
Alexander Wilms <alex.wilms@adminguru.org>
2017-10-27 23:44:35 +0200

    Workaround for https://github.com/bblanchon/ArduinoJson/issues/566

--- node-wifi-mqtt-homie-battery/node-wifi-mqtt-homie-battery.ino
+++ node-wifi-mqtt-homie-battery/node-wifi-mqtt-homie-battery.ino
@@ -33,27 +33,71 @@
 
 **********************************************************************************************************/
 
+/*
+ * Some notes to increase battery runtime:
+ * You should remove and short out the input polarity protection diode 
+ * if you are using this sketch with the PCB/Design from 
+ * https://www.imker-nettetal.de/bienen-nsa/ESP8266-BeeScale1_1.fzz
+ * to avoid unnecessary voltage drop. But don't mix polarity!
+ * 
+ * Also you should remove any unnecessary power indicator LED to save
+ * a couple om mA. (e.g. on ESP-07 the red power indicator LED)
+ */
+
 #include <Homie.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include "HX711.h"
 #include <RunningMedian.h>
+#include <Ticker.h>
 
-HX711 scale;
+#define FW_NAME "node-wifi-mqtt-homie-battery"
+#define FW_VERSION "0.9.5"
+const int DEFAULT_SLEEP_TIME = 600;
 
-const int DEFAULT_SLEEP_TIME = 60;
+//Workaround for https://github.com/bblanchon/ArduinoJson/issues/566
+#define ARDUINOJSON_USE_DOUBLE 0
+//function to round to two decimals
+float round2two(float tmp) {
+  return ((int)(tmp * 100)) / 100.00;
+}
+
+// PINs
+#define ONE_WIRE_BUS 14
+#define DOUT  13
+#define PD_SCK  12
 
 /* Use sketch BeeScale-Calibration.ino to determine these calibration values.
    Set them here or use HomieSetting via config.json or WebApp/MQTT
 */
-const float DEFAULT_WEIGHT_OFFSET = 244017.00; // Load cell zero offset. 
-const float DEFAULT_KILOGRAM_DIVIDER = 22.27;  // Load cell value per kilogram.
+const long DEFAULT_WEIGHT_OFFSET = 666; // Load cell zero offset. 
+const float DEFAULT_KILOGRAM_DIVIDER = 21.59;  // Load cell value per kilogram.
+const float DEFAULT_CALIBRATION_TEMPERATURE = 23.44; // Temperature at which the scale has been calibrated for Temperature compensation
+const float DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE = 0.0; // Calibration factor in gram per degree
+
+/*
+ * Scale should be calibrated with a regulated input of 3.3V!
+ * Every change during runtime will cause wrong readings
+ */
+const float LOW_BATTERY = 3.1;
+
+/*
+ * *Some* ESPs tend to have a too low reading on ESP.getVcc()
+ * To get the maximum battery runtime you should check what ADC is reading and compare
+ * it with a multimeter between VCC and GND at the ESP. Change the following value to e.g. 0.13
+ * or -0.02 in case of a too high reading. Or set it using HomieSetting via config.json or WebApp/MQTT
+ */
+const float DEFAULT_VCC_ADJUST = -0.21;
 
 HomieSetting<long> sleepTimeSetting("sleepTime", "SleepTime in seconds (max. 3600s!), default is 60s");
-HomieSetting<double> weightOffsetSetting("weightOffset", "Offset value to zero. Use BeeScale-Calibration.ino to determine.");
+HomieSetting<long> weightOffsetSetting("weightOffset", "Offset value to zero. Use BeeScale-Calibration.ino to determine.");
 HomieSetting<double> kilogramDividerSetting("kilogramDivider", "Scale value per kilogram. Use BeeScale-Calibration.ino to determine.");
+HomieSetting<double> calibrationTemperatureSetting("calibrationTemperature", "Outside Temperature at which the scale has been calibrated");
+HomieSetting<double> calibrationFactorSetting("calibrationFactor", "Calibration Factor in gram per degree. 0.0 to disable adjustment");
+HomieSetting<double> vccAdjustSetting("vccAdjust", "Calibration value for input voltage. See sketch for details.");
 
+HX711 scale;
 
 ADC_MODE(ADC_VCC);
 
@@ -72,11 +116,14 @@ extern "C" {
 
 #define STATE_COLDSTART 0
 #define STATE_SLEEP_WAKE 1
-#define STATE_ALARM 2
 #define STATE_CONNECT_WIFI 4
 
+// For connection loss detection, timeout in seconds
+#define RUNTIME_MAX 15
+int runtime_s = 0;
+Ticker timeout;
+
 #define BATT_WARNING_VOLTAGE 3.2          // Voltage for Low-Bat warning
-//#define WIFI_CONNECT_TIMEOUT_S 20         // max time for wifi connect to router, if exceeded reset //FIXME
 
 // RTC-MEM Adresses
 #define RTC_BASE 65
@@ -94,27 +141,32 @@ int i;
 
 //end deep sleep infrastructure
 
-RunningMedian WeightSamples = RunningMedian(4);
+RunningMedian WeightSamples = RunningMedian(3);
 
 float weight;
 float temperature0;
 float temperature1;
 float voltage;
+float raw_voltage;
+float vcc_adjust;
+int SLEEP_TIME;
 
 HomieNode weightNode("weight", "weight");
 HomieNode temperatureNode0("temperature0", "temperature");
 HomieNode temperatureNode1("temperature1", "temperature");
-HomieNode batteryNode("battery", "battery");
-HomieNode jsonNode("data","__json__");
+HomieNode batteryNode("battery", "volt");
+HomieNode batAlarmNode("battery","alarm");
+HomieNode jsonNode("data","__json__"); //Hiveeyes.org compatibility format
 
 void setupHandler() {
+  delay(500);
   weightNode.setProperty("unit").send("kg");
   temperatureNode0.setProperty("unit").send("C");
   temperatureNode1.setProperty("unit").send("C");
   batteryNode.setProperty("unit").send("V");
+  batAlarmNode.setProperty("bool");
 }
 
-#define ONE_WIRE_BUS 14
 // Setup a OneWire instance to communicate with any OneWire devices
 OneWire OneWire(ONE_WIRE_BUS);
 // Pass our OneWire reference to Dallas Temperature.
@@ -140,75 +192,149 @@ void getTemperatures() {
 }
 
 void getWeight() {
-  scale.begin(13, 12);
+  scale.begin(DOUT, PD_SCK);
   scale.set_scale(kilogramDividerSetting.get());  //initialize scale
+  Homie.getLogger() << "DEBUG: Got kilogramDivider" << kilogramDividerSetting.get() << endl;
   scale.set_offset(weightOffsetSetting.get());    //initialize scale
-  Serial.println("Getting weight, hold on");
-  scale.power_up();
-  for (int i = 0; i < 4; i++) {
-    Serial.print("*");
-    float WeightRaw = scale.get_units(10);
-    yield();
+  Homie.getLogger() << "DEBUG: Got weightOffset: " << weightOffsetSetting.get() << endl;
+  Homie.getLogger() << endl << endl;
+  Homie.getLogger() << "Reading scale, hold on" << endl;
+  for (int i = 0; i < 3; i++) {
+    scale.power_up();
+    float WeightRaw = scale.get_units(3);
+    scale.power_down();
+    //yield();
+    Homie.getLogger() << "✔ Raw measurements: " << WeightRaw << " g" << endl;
     WeightSamples.add(WeightRaw);
     delay(500);                 
     yield();
   }
-  scale.power_down();
-  Serial.println("");
   
   weight = WeightSamples.getMedian();
+
+  //temperature compensation
+  Homie.getLogger() << "✔ uncompensated median Weight: " << weight << " g" << endl;
+  float calibrationTemperature = calibrationTemperatureSetting.get();
+  float calibrationFactor = calibrationFactorSetting.get();
+  if (temperature1 < calibrationTemperature) weight = weight+(fabs(temperature1 - calibrationTemperature)*calibrationFactor);
+  if (temperature1 > calibrationTemperature) weight = weight-(fabs(temperature1 - calibrationTemperature)*calibrationFactor);
+  Homie.getLogger() << "✔ compensated median Weight: " << weight << "g" << endl;
+
   weight = weight / 1000 ;      // we want kilogram
 }
 
-void loopHandler() {
-    //getWeight();
-    Serial << "Weight: " << weight << " kg" << endl;
+void getVolt() {
+    raw_voltage = ESP.getVcc();
+    vcc_adjust = vccAdjustSetting.get();
+    Homie.getLogger() << "Voltage adjust value is: " << vcc_adjust << "V" << endl;
+    voltage = raw_voltage / 1000 + vcc_adjust;
+}
+        
+void transmit() {
+    //debug MTU
+    delay(100);
+    Homie.getLogger() << "Weight: " << weight << " kg" << endl;
     weightNode.setProperty("kilogram").setRetained(false).send(String(weight));
-
-    //getTemperatures();
-    Serial << "Temperature0: " << temperature0 << " °C" << endl;
+    
+    Homie.getLogger() << "Temperature0: " << temperature0 << " °C" << endl;
     temperatureNode0.setProperty("degrees").setRetained(false).send(String(temperature0));
 
-    Serial << "Temperature1: " << temperature1 << " °C" << endl;
+
+    Homie.getLogger() << "Temperature1: " << temperature1 << " °C" << endl;
     temperatureNode1.setProperty("degrees").setRetained(false).send(String(temperature1));
 
-    voltage = ESP.getVcc() / 1000 + 0.3; // ESP07 reads 0.3V too low
-    Serial << "Input Voltage: " << voltage << " V" << endl;
+
+    Homie.getLogger() << "Raw Input Voltage: " << raw_voltage / 1000<< " V" << endl;
+    Homie.getLogger() << "Corrected Input Voltage: " << voltage << " V" << endl;
     batteryNode.setProperty("volt").setRetained(true).send(String(voltage));
 
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
-    root["Weight"] = weight;
-    root["Temp1"] = temperature0;
-    root["Temp2"] = temperature1;
-    root["VCC"] = voltage; 
+    root["Weight"] = round2two(weight);
+    root["Temp1"] = round2two(temperature0);
+    root["Temp2"] = round2two(temperature1);
+    root["VCC"] = round2two(voltage); 
     String values;
     root.printTo(values);
-    Serial << "Json data:" << values << endl;
+    Homie.getLogger() << "Json data:" << values << endl;
     jsonNode.setProperty("__json__").setRetained(false).send(values);
 
+    /*
+     * determine low battery, sleep forever to save lithium cell.
+     * we could go lower, but scale is calibrated at 3.3V so we    
+     * would get wrong readings.
+    */
+    if (voltage <= LOW_BATTERY)
+    {
+      batAlarmNode.setProperty("alarm").setRetained(true).send("true");
+      SLEEP_TIME = 0;
+      Homie.getLogger() << F("✖✖✖ Battery critically low, will sleep forever ✖✖✖") << endl;
+    }else
+    {
+      batAlarmNode.setProperty("alarm").setRetained(true).send("false");
+      SLEEP_TIME = sleepTimeSetting.get();
+      Homie.getLogger() << "✔ Preparing for " << SLEEP_TIME << " seconds sleep" << endl;
+    }
+}
 
-    Homie.prepareToSleep();
-    delay(500);
-    Serial << "Ready to sleep" << endl;
+void max_run() {
+  ++runtime_s;
+  if (runtime_s == RUNTIME_MAX){
+    Homie.getLogger() << "DEBUG: Max. runtime of " << RUNTIME_MAX << "s reached, shutting down!" << endl;
+    /*
     buf[0] = STATE_SLEEP_WAKE;
     system_rtc_mem_write(RTC_STATE, buf, 1); // set state for next wakeUp
-    //FIXME
-    int SLEEP_TIME = sleepTimeSetting.get();
-    Serial << "Sleeping for " << SLEEP_TIME << " seconds" << endl;
-    ESP.deepSleep(SLEEP_TIME*1000000, WAKE_RF_DISABLED);
-    yield();
+    ESP.deepSleep(SLEEP_TIME*1000000, WAKE_RF_DISABLED);    
+    */
+    SLEEP_TIME = sleepTimeSetting.get();
+    Homie.getLogger() << "✔ Preparing for " << SLEEP_TIME << " seconds sleep" << endl;
+    Homie.prepareToSleep();
+  }
+  
+}
+
+
+void onHomieEvent(const HomieEvent& event) {
+  switch(event.type) {
+    case HomieEventType::NORMAL_MODE:
+
+    timeout.attach(1.0, max_run);
+    
+    getTemperatures();
+    getWeight();
+    getVolt();
+    Homie.getLogger() << "DEBUG: After measurements: " << millis() / 1000 << endl;
+    break;
+    
+    case HomieEventType::MQTT_READY:
+    //debug MTU?
+      delay(100);
+      transmit();
+      Homie.getLogger() << "DEBUG: After transmit(): " << millis() / 1000 << endl;
+      Homie.getLogger() << "✔ Data is transmitted, waiting for package acks..." << endl;
+      Homie.prepareToSleep();
+    break;
+
+    case HomieEventType::READY_TO_SLEEP:
+      Homie.getLogger() << "DEBUG: Total runtime: " << millis() / 1000 << endl;
+      buf[0] = STATE_SLEEP_WAKE;
+      system_rtc_mem_write(RTC_STATE, buf, 1); // set state for next wakeUp
+      ESP.deepSleep(SLEEP_TIME*1000000, WAKE_RF_DISABLED);
+    break;
+  }
+}
+
+void loopHandler() {
 }
 
 void setup() {
-
+   
   WiFi.forceSleepBegin();  // send wifi directly to sleep to reduce power consumption
   yield();
   system_rtc_mem_read(RTC_BASE, buf, 2); // read 2 bytes from RTC-MEMORY to get our state
   
   Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+  Homie.getLogger() << endl << endl;
 
   if ((buf[0] != 0x55) || (buf[1] != 0xaa))  // cold start, magic number is not present in rtc
   { state = STATE_COLDSTART;
@@ -234,7 +360,7 @@ void setup() {
       yield();
       break;
 
-        case STATE_SLEEP_WAKE:
+    case STATE_SLEEP_WAKE:
       // prepare to activate wifi
       buf[0] = STATE_CONNECT_WIFI;  // one more sleep required to to wake with wifi on
       WiFi.forceSleepWake();
@@ -244,21 +370,28 @@ void setup() {
       yield();
       break;      
   
-    case STATE_CONNECT_WIFI:
-      sleepTimeSetting.setDefaultValue(DEFAULT_SLEEP_TIME);
+    case STATE_CONNECT_WIFI:  
+      Homie.disableLedFeedback(); // collides with Serial on ESP07
+      Homie.onEvent(onHomieEvent);
+     
+     sleepTimeSetting.setDefaultValue(DEFAULT_SLEEP_TIME);
+    /*
+     * Can't set more than one value to default, see issue #323
+     * https://github.com/marvinroger/homie-esp8266/issues/323
+     */
       weightOffsetSetting.setDefaultValue(DEFAULT_WEIGHT_OFFSET);
       kilogramDividerSetting.setDefaultValue(DEFAULT_KILOGRAM_DIVIDER);
-      
-      voltage = ESP.getVcc() / 1000;
-      getTemperatures();
-      getWeight();
-      
+      vccAdjustSetting.setDefaultValue(DEFAULT_VCC_ADJUST);
+      calibrationTemperatureSetting.setDefaultValue(DEFAULT_CALIBRATION_TEMPERATURE);
+      calibrationFactorSetting.setDefaultValue(DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE);
+
+      Homie.getLogger() << "DEBUG: Turn on WIFI: " << millis() / 1000 << endl;
       WiFi.forceSleepWake();
       delay(500);
       wifi_set_sleep_type(MODEM_SLEEP_T);
         
-      Homie_setFirmware("node-wifi-mqtt-homie-battery", "1.0.5");
-      Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+      Homie_setFirmware(FW_NAME, FW_VERSION);
+      Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);  
 
       weightNode.advertise("unit");
       weightNode.advertise("kilogram");
@@ -272,8 +405,8 @@ void setup() {
       batteryNode.advertise("unit");
       batteryNode.advertise("volt");
 
+      Homie.getLogger() << "DEBUG: Before setup(): " << millis() / 1000 << endl;
 
-      Homie.disableLedFeedback(); // collides with Serial on ESP07
       Homie.setup();
       break;
     }
--- node-wifi-mqtt-homie/node-wifi-mqtt-homie.ino
+++ node-wifi-mqtt-homie/node-wifi-mqtt-homie.ino
@@ -43,20 +43,49 @@
 
 HX711 scale;
 
+#define FW_NAME "node-wifi-mqtt-homie"
+#define FW_VERSION "0.9.5"
 const int DEFAULT_SEND_INTERVAL = 60;
+
+//Workaround for https://github.com/bblanchon/ArduinoJson/issues/566
+#define ARDUINOJSON_USE_DOUBLE 0
+//function to round to two decimals
+float round2two(float tmp) {
+  return ((int)(tmp * 100)) / 100.00;
+}
+
+// PINs
+#define ONE_WIRE_BUS 14
+#define DOUT  13
+#define PD_SCK  12
 
 /* Use sketch BeeScale-Calibration.ino to determine these calibration values.
    Set them here or use HomieSetting via config.json or WebApp/MQTT
 */
 const float DEFAULT_WEIGHT_OFFSET = 244017.00; // Load cell zero offset. 
 const float DEFAULT_KILOGRAM_DIVIDER = 22.27;  // Load cell value per kilogram.
+const float DEFAULT_CALIBRATION_TEMPERATURE = 20.0; // Temperature at which the scale has been calibrated for Temperature compensation
+const float DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE = 0.0; // Calibration factor in gram per degree
 unsigned long lastSent = 0;
 
 RunningMedian WeightSamples = RunningMedian(4);
+
 float weight;
+float temperature0;
+float temperature1;
+float voltage;
+float raw_voltage;
+float vcc_adjust;
+int sendInterval;
+
 
 ADC_MODE(ADC_VCC);
-float volt;
+
+// Setup a OneWire instance to communicate with any OneWire devices
+OneWire OneWire(ONE_WIRE_BUS);
+// Pass our OneWire reference to Dallas Temperature.
+DallasTemperature sensors(&OneWire);
+
 
 HomieNode temperatureNode0("temperature0", "temperature");
 HomieNode temperatureNode1("temperature1", "temperature");
@@ -65,8 +94,11 @@ HomieNode batteryNode("battery", "battery");
 HomieNode jsonNode("data","__json__");
 
 HomieSetting<long> sendIntervalSetting("sendInterval", "Interval for measurements in seconds (max. 3600)");
-HomieSetting<double> weightOffsetSetting("weightOffset", "Offset value to zero. Use BeeScale-Calibration.ino to determine.");
+HomieSetting<long> weightOffsetSetting("weightOffset", "Offset value to zero. Use BeeScale-Calibration.ino to determine.");
 HomieSetting<double> kilogramDividerSetting("kilogramDivider", "Scale value per kilogram. Use BeeScale-Calibration.ino to determine.");
+HomieSetting<double> calibrationTemperatureSetting("calibrationTemperature", "Outside Temperature at which the scale has been calibrated");
+HomieSetting<double> calibrationFactorSetting("calibrationFactor", "Calibration Factor in gram per degree. 0.0 to disable adjustment");
+HomieSetting<double> vccAdjustSetting("vccAdjust", "Calibration value for input voltage. See sketch for details.");
 
 void setupHandler() {
   temperatureNode0.setProperty("unit").send("C");
@@ -74,14 +106,6 @@ void setupHandler() {
   weightNode.setProperty("unit").send("kg");
   batteryNode.setProperty("unit").send("V");
 }
-
-#define ONE_WIRE_BUS 14
-// Setup a OneWire instance to communicate with any OneWire devices
-OneWire OneWire(ONE_WIRE_BUS);
-// Pass our OneWire reference to Dallas Temperature.
-DallasTemperature sensors(&OneWire);
-float temperature0;
-float temperature1;
 
 void getTemperatures() {
   sensors.begin();
@@ -102,48 +126,70 @@ void getTemperatures() {
 }
 
 void getWeight() {
-  Serial.println("Getting weight, hold on for 4 seconds");
-    
-  for (int i = 0; i < 4; i++) {
-    Serial.print("*");
-    scale.power_up();
-    float WeightRaw = scale.get_units(10);
+  scale.set_scale(kilogramDividerSetting.get());  //initialize scale
+  scale.set_offset(weightOffsetSetting.get());    //initialize scale
+  Homie.getLogger() << endl << endl;
+  Homie.getLogger() << "Reading scale, hold on" << endl;
+  scale.power_up();
+  for (int i = 0; i < 3; i++) {
+    float WeightRaw = scale.get_units(3);
     yield();
-    scale.power_down();
+    Homie.getLogger() << "✔ Raw measurements: " << WeightRaw << "g" << endl;
     WeightSamples.add(WeightRaw);
-    delay(1000);                 
+    delay(500);                 
     yield();
   }
-  Serial.println("");
-  weight = WeightSamples.getMedian() / 1000;
+  scale.power_down();
+  
+  weight = WeightSamples.getMedian();
+
+  //temperature compensation
+  Homie.getLogger() << "✔ uncompensated median Weight: " << weight << "g" << endl;
+  float calibrationTemperature = calibrationTemperatureSetting.get();
+  float calibrationFactor = calibrationFactorSetting.get();
+  if (temperature1 < calibrationTemperature) weight = weight+(fabs(temperature1 - calibrationTemperature)*calibrationFactor);
+  if (temperature1 > calibrationTemperature) weight = weight-(fabs(temperature1 - calibrationTemperature)*calibrationFactor);
+  Homie.getLogger() << "✔ compensated median Weight: " << weight << "g" << endl;
+
+  weight = weight / 1000 ;      // we want kilogram
+}
+
+void getVolt() {
+    raw_voltage = ESP.getVcc();
+    vcc_adjust = vccAdjustSetting.get();
+    Homie.getLogger() << "Voltage adjust value is: " << vcc_adjust << "V" << endl;
+    voltage = raw_voltage / 1000 + vcc_adjust;
 }
 
 void loopHandler() {
   if (millis() - lastSent >= sendIntervalSetting.get() * 1000UL || lastSent == 0) {
     getTemperatures();
-    Serial << "Temperature0: " << temperature0 << " °C" << endl;
+    Homie.getLogger() << "Temperature0: " << temperature0 << " °C" << endl;
     temperatureNode0.setProperty("degrees").setRetained(false).send(String(temperature0));
 
-    Serial << "Temperature1: " << temperature1 << " °C" << endl;
+    Homie.getLogger() << "Temperature1: " << temperature1 << " °C" << endl;
     temperatureNode1.setProperty("degrees").setRetained(false).send(String(temperature1));
 
     getWeight();
-    Serial << "Weight: " << weight << " kg" << endl;
+    Homie.getLogger() << "Weight: " << weight << " kg" << endl;
     weightNode.setProperty("kilogram").setRetained(false).send(String(weight));
 
-    volt = ESP.getVcc() / 1000 + 0.3; // ESP07 reads 0.3V too low
-    Serial << "Input Voltage: " << volt << " V" << endl;
-    batteryNode.setProperty("volt").setRetained(true).send(String(volt));
+    getVolt();
+    Homie.getLogger() << "Raw Input Voltage: " << raw_voltage << " V" << endl;
+    Homie.getLogger() << "Input Voltage: " << voltage << " V" << endl;
+    batteryNode.setProperty("volt").setRetained(true).send(String(voltage));
 
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
-    root["Weight"] = weight;
-    root["Temp1"] = temperature0;
-    root["Temp2"] = temperature1;
+    root["Weight"] = round2two(weight);
+    root["Temp1"] = round2two(temperature0);
+    root["Temp2"] = round2two(temperature1);
     String values;
     root.printTo(values);
-    Serial << "Json data:" << values << endl;
+    Homie.getLogger() << "Json data:" << values << endl;
     jsonNode.setProperty("__json__").setRetained(false).send(values);
+
+    //Homie.getLogger() << "Heap size: " << ESP.getFreeHeap() << " bytes, ";
     
     lastSent = millis();
   }
@@ -151,20 +197,22 @@ void loopHandler() {
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println();
-  Homie_setFirmware("node-wifi-mqtt-homie", "1.0.5");
+
+  Homie_setFirmware(FW_NAME, FW_VERSION);
   Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
   Homie.disableLedFeedback(); // LED pin would break serial on ESP-07
 
-  
   sendIntervalSetting.setDefaultValue(DEFAULT_SEND_INTERVAL);
+  /*
+  * Can't set more than one value to default, see issue #323
+  * https://github.com/marvinroger/homie-esp8266/issues/323
+  */
   weightOffsetSetting.setDefaultValue(DEFAULT_WEIGHT_OFFSET);
   kilogramDividerSetting.setDefaultValue(DEFAULT_KILOGRAM_DIVIDER);
+  calibrationTemperatureSetting.setDefaultValue(DEFAULT_CALIBRATION_TEMPERATURE);
+  calibrationFactorSetting.setDefaultValue(DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE);
 
-  scale.begin(13, 12);
-  scale.set_scale(kilogramDividerSetting.get());
-  scale.set_offset(weightOffsetSetting.get());    
+  scale.begin(DOUT, PD_SCK);
 
   temperatureNode0.advertise("unit");
   temperatureNode0.advertise("degrees");
